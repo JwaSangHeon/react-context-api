fcde2e774584f4cba158765b7d561ebe4ca0baf5
JwaSangHeon <whk4051@naver.com>
2022-05-24 11:39:11 +0900

    Provider, Consumer 이해

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+import SelectColors from "./components/SelectColors";
+import { ColorProvider } from "./contexts/color";
 
 function App() {
   return (
-    //Provider은 value값을 명시해 주어야만 한다.
-    <ColorContext.Provider value={{ color: "red" }}>
+    <ColorProvider>
+      <SelectColors />
       <ColorBox />
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 }
 
